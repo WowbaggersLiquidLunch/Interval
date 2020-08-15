8729d6517dd2ce6162a35b77be2c44d43f5cd9d7
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-08-15 15:49:28 -0400

    test Interval operators directly against initialisers

---
+++ Tests/IntervalTests/IntervalInitialisationTests.swift
@@ -0,0 +1,214 @@
+import XCTest
+@testable import Interval
+
+///	A group of test cases and methods regarding instantiating `Interval`.
+final class IntervalInitialisationTests: XCTestCase {
+	
+	///	Checks that interval operators work as intended.
+	func testIntervalOperators() {
+		
+		//	MARK: Ascending Interval operators.
+		
+		XCTAssertEqual(
+			0≤∙≤1,
+			Interval(
+				lowerBoundary: .closed,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .closed,
+				inInverseStridingDirection: false
+			),
+			"`0≤∙≤1` fails to initialise [0, 1]."
+		)
+		
+		XCTAssertEqual(
+			0≤∙<1,
+			Interval(
+				lowerBoundary: .closed,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .open,
+				inInverseStridingDirection: false
+			),
+			"`0≤∙<1` fails to initialise [0, 1)."
+		)
+		
+		XCTAssertEqual(
+			0<∙≤1,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .closed,
+				inInverseStridingDirection: false
+			),
+			"`0<∙≤1` fails to initialise (0, 1]."
+		)
+		
+		XCTAssertEqual(
+			0<∙<1,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .open,
+				inInverseStridingDirection: false
+			),
+			"`0<∙<1` fails to initialise (0, 1)."
+		)
+		
+		XCTAssertEqual(
+			∙∙≤1,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .unbounded,
+				upperEndpoint: .bounded(1),
+				upperBoundary: .closed,
+				inInverseStridingDirection: false
+			),
+			"`∙∙≤1` fails to initialise (-∞, 1]."
+		)
+		
+		XCTAssertEqual(
+			∙∙<1,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .unbounded,
+				upperEndpoint: .bounded(1),
+				upperBoundary: .open,
+				inInverseStridingDirection: false
+			),
+			"`∙∙<1` fails to initialise (-∞, 1)."
+		)
+		
+		XCTAssertEqual(
+			0≤∙∙,
+			Interval(
+				lowerBoundary: .closed,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .unbounded,
+				upperBoundary: .open,
+				inInverseStridingDirection: false
+			),
+			"`0≤∙∙` fails to initialise [0, ∞)."
+		)
+		
+		XCTAssertEqual(
+			0<∙∙,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .unbounded,
+				upperBoundary: .open,
+				inInverseStridingDirection: false
+			),
+			"`0<∙∙` fails to initialise (0, ∞)."
+		)
+		
+		//	MARK: Descending interval operators.
+		
+		XCTAssertEqual(
+			1≥∙≥0,
+			Interval(
+				lowerBoundary: .closed,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .closed,
+				inInverseStridingDirection: true
+			),
+			"`1≥∙≥0` fails to initialise inverse [0, 1]."
+		)
+		
+		XCTAssertEqual(
+			1>∙≥0,
+			Interval(
+				lowerBoundary: .closed,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .open,
+				inInverseStridingDirection: true
+			),
+			"`1>∙≥0` fails to initialise inverse [0, 1)."
+		)
+		
+		XCTAssertEqual(
+			1≥∙>0,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .closed,
+				inInverseStridingDirection: true
+			),
+			"`1≥∙>0` fails to initialise inverse (0, 1]."
+		)
+		
+		XCTAssertEqual(
+			1>∙>0,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .bounded(1),
+				upperBoundary: .open,
+				inInverseStridingDirection: true
+			),
+			"`1>∙>0` fails to initialise inverse (0, 1)."
+		)
+		
+		XCTAssertEqual(
+			1≥∙∙,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .unbounded,
+				upperEndpoint: .bounded(1),
+				upperBoundary: .closed,
+				inInverseStridingDirection: true
+			),
+			"`1≥∙∙` fails to initialise inverse (-∞, 1]."
+		)
+		
+		XCTAssertEqual(
+			1>∙∙,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .unbounded,
+				upperEndpoint: .bounded(1),
+				upperBoundary: .open,
+				inInverseStridingDirection: true
+			),
+			"`1>∙∙` fails to initialise inverse (-∞, 1)."
+		)
+		
+		XCTAssertEqual(
+			∙∙≥0,
+			Interval(
+				lowerBoundary: .closed,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .unbounded,
+				upperBoundary: .open,
+				inInverseStridingDirection: true
+			),
+			"`∙∙≥0` fails to initialise inverse [0, ∞)."
+		)
+		
+		XCTAssertEqual(
+			∙∙>0,
+			Interval(
+				lowerBoundary: .open,
+				lowerEndpoint: .bounded(0),
+				upperEndpoint: .unbounded,
+				upperBoundary: .open,
+				inInverseStridingDirection: true
+			),
+			"`∙∙>0` fails to initialise inverse (0, ∞)."
+		)
+		
+	}
+	
+	///	An array of all test cases and methods regarding instantiating `Interval`.
+	///
+	///	This constant informs `XCTestManifests.swift` of what cases there are to test.
+	static let allTests = [
+		("testIntervalOperators", testIntervalOperators),
+	]
+}
