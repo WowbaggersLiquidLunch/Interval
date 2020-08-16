b6c00e55f74fd7e3b74904046b8f842920f9a92a
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-08-16 15:13:23 -0400

    extract common declarations

--- Tests/IntervalTests/Interval Boundary Independence Tests.swift
+++ Tests/IntervalTests/Interval Boundary Independence Tests.swift
@@ -10,32 +10,25 @@ import XCTest
 ///	A group of test cases and methods regarding `IntervalBoundaryAccessibility` and `IntervalBoundaryAvailability`'s independence from `Interval`.
 final class IntervalBoundaryIndependenceTests: XCTestCase {
 	
+	let intervalOfInts: Interval<Int> = 0≤∙≤1
+	let intervalOfDoubles: Interval<Double> = 2.7≤∙≤3.14
+	
 	///	Checks that `IntervalBoundaryAccessibility` is not bounded by `Interval.Member`.
 	func testIndependenceOfIntervalBoundaryAccessibilityFromInterval() {
-		
-		let intervalOfInts: Interval<Int> = 0≤∙≤1
-		let intervalOfDoubles: Interval<Double> = 2.7≤∙≤3.14
-		
 		XCTAssertEqual(
 			intervalOfInts.lowerBoundaryAccessibility,
 			intervalOfDoubles.lowerBoundaryAccessibility,
 			"`IntervalBoundaryAccessibility` fails to be independent from `Interval`."
 		)
-		
 	}
 	
 	///	Checks that `IntervalBoundaryAvailability` is not bounded by `Interval.Member`.
 	func testIndependenceOfIntervalBoundaryAvailabilityFromInterval() {
-
-		let intervalOfInts: Interval<Int> = 0≤∙≤1
-		let intervalOfDoubles: Interval<Double> = 2.7≤∙≤3.14
-
 		XCTAssertEqual(
 			intervalOfInts.lowerBoundaryAvailability,
 			intervalOfDoubles.lowerBoundaryAvailability,
 			"`IntervalBoundaryAvailability` fails to be independent from `Interval`."
 		)
-
 	}
 	
 }
